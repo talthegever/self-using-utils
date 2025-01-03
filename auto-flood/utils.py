1b20044b2bb69a8c481292ba032c5111a6c7c1bb
talthegever <tal03b@gmail.com>
2025-01-03 15:52:00 +0200

    add two methods

--- auto-flood/utils.py
+++ auto-flood/utils.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, date, timedelta
 
 
 class DateUtils:
@@ -9,29 +9,29 @@ class DateUtils:
         :param day: "2023-09-15" like format
         :return: datetime of this day
         """
-        return datetime.date.fromisoformat(day)
+        return date.fromisoformat(day)
 
     @staticmethod
-    def get_closest_valid_date(date: datetime.date) -> datetime.date:
+    def get_closest_valid_date(specific_date: date) -> datetime.date:
         """
-        :param date: datetime.date
+        :param specific_date: datetime.date
         :return: itself. if sunday or saturday, returns last friday
         """
-        while date.weekday() > 4:
-            date = date - datetime.timedelta(days=1)
-        return date
+        while specific_date.weekday() > 4:
+            specific_date = specific_date - timedelta(days=1)
+        return specific_date
 
     @staticmethod
-    def get_previous_valid_date(date: datetime.date) -> datetime.date:
+    def get_previous_valid_date(specific_date: date) -> datetime.date:
         """
-        :param date: datetime.date
+        :param specific_date: datetime.date
         :return: yesterday. if sunday or saturday, return last Thursday
         """
-        return DateUtils.get_closest_valid_date(date - datetime.timedelta(days=1))
+        return DateUtils.get_closest_valid_date(specific_date - timedelta(days=1))
 
     @staticmethod
-    def is_stocks_day(date: datetime.date) -> bool:
-        return date.weekday() <= 4
+    def is_stocks_day(specific_date: datetime.date) -> bool:
+        return specific_date.weekday() <= 4
 
     @staticmethod
     def get_date_x_days_back_from_now(days: int) -> datetime.date:
@@ -39,7 +39,15 @@ class DateUtils:
         :param days: days back
         :return: datetime.date - wanted date
         """
-        return datetime.date.today() - datetime.timedelta(days=days)
+        return date.today() - timedelta(days=days)
+
+    @staticmethod
+    def get_first_day_of_year(year: datetime.year):
+        return datetime(year, 1, 1)
+
+    @staticmethod
+    def get_first_day_of_month(month: datetime.month, year: datetime.year = datetime.now().year):
+        return datetime(year, month, 1)
 
 
 class PrintUtils:
